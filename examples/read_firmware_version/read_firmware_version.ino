4406cedea7ea24759f8ec108d8089f4769cbdf70
Baozhu Zuo <zuobaozhu@gmail.com>
2020-02-02 12:45:52 +0800

    Pretty printed the Arduino code with astyle

--- examples/read_firmware_version/read_firmware_version.ino
+++ examples/read_firmware_version/read_firmware_version.ino
@@ -4,22 +4,20 @@
 
 Multi_Channel_Relay relay;
 
-void setup()
-{
-  DEBUG_PRINT.begin(9600);
-  while(!DEBUG_PRINT);   
+void setup() {
+    DEBUG_PRINT.begin(9600);
+    while (!DEBUG_PRINT);
 
-	// Set I2C address and start relay
-  relay.begin(0x11);
+    // Set I2C address and start relay
+    relay.begin(0x11);
 
-  /* Read firmware  version */
-  DEBUG_PRINT.print("firmware version: ");
-  DEBUG_PRINT.print("0x");
-  DEBUG_PRINT.print(relay.getFirmwareVersion(), HEX);
-  DEBUG_PRINT.println();
+    /* Read firmware  version */
+    DEBUG_PRINT.print("firmware version: ");
+    DEBUG_PRINT.print("0x");
+    DEBUG_PRINT.print(relay.getFirmwareVersion(), HEX);
+    DEBUG_PRINT.println();
 }
 
-void loop()
-{
+void loop() {
 
 }
